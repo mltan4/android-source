0437edea0668c0781bcfc9a8fae24aa8df47e7ae
Mark Tan <markjosephltan@gmail.com>
2014-05-20 21:54:15 -0700

    Completed Generics

--- milestone_01_java/01_polymorphism/generics/src/com/bloc/generics/Main.java
+++ milestone_01_java/01_polymorphism/generics/src/com/bloc/generics/Main.java
@@ -9,8 +9,23 @@ public class Main extends Object {
 		/*
 		 * Put a bunch of Toys in the toybox!
 		 */
-		ToyBox toyBox = new ToyBox();
+		
+		Toy<ActionFigure> actionfigure1 = new Toy<ActionFigure>(new ActionFigure());
+		// Nick's awesome code-to-human guide for dummies
+		// Toy data type with ActionFigure associated as the first generic type called af1
+		// then assign a new instance of Toy with matching data type -- this requires a param that matches the first gen type 
+		// The first gen type is ActionFigure. Then we create a new instance of ActionFigure which requires no param in its constructor 
+		Toy<Book> book1 = new Toy<Book>(new Book());
+		Toy<Spoon> spoon1 = new Toy<Spoon>(new Spoon());
+		Toy<Thing> thing1 = new Toy<Thing>(new Thing("rubiks cube"));
 
+		ToyBox toyBox = new ToyBox();
+		
+		toyBox.addToy(actionfigure1);
+		toyBox.addToy(book1);		
+		toyBox.addToy(spoon1);
+		toyBox.addToy(thing1);
+		
 		if (toyBox.getToyCount() == 0) {
 			System.out.println("Let's get some toys in that box!");
 			System.exit(1);
