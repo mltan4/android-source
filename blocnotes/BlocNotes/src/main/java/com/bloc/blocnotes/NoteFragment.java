a10eabc9b8a0e1b1648e0397fbc1a13a5a24700d
Mark Tan <markjosephltan@gmail.com>
2014-05-26 18:47:28 -0700

    Created a brand new Class, NoteFragment

---
+++ blocnotes/BlocNotes/src/main/java/com/bloc/blocnotes/NoteFragment.java
@@ -0,0 +1,18 @@
+package com.bloc.blocnotes;
+
+import android.app.Fragment;
+
+/**
+ * Created by mark.tan on 5/26/14.
+ */
+public class NoteFragment extends Fragment{
+    NoteFragment(){
+        Fragment f = new Fragment();
+    }
+}
+
+// Instantiating an object
+// Instance of a class
+// Class Fragment will have a new instance by calling Fragment constructor
+// Assign this object to variable f
+// f refers to an instance of Fragment class
